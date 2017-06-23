07047144fc2427369f97c6f8c6908e3fa73b7df4
minerva22 <minerva.moawad@gmail.com>
2017-06-23 11:27:14 +0300

    put echo

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
 //run python with shell exec()
-$x=shell_exec("python /home/minerva/Desktop/programming/django/jobsproj/manage.py which_email Debitors notice - LB")
+ echo shell_exec("python /home/minerva/Desktop/programming/django/jobsproj/manage.py which_email Debitors notice - LB");
 
 
 
 
-?>
+
