d6a593664e1bb0c223af425b61e84aadfecc438e
minerva22 <minerva.moawad@gmail.com>
2017-06-28 16:09:47 +0300

    try some code to get the emails without html code using crul

--- index.php
+++ index.php
@@ -8,12 +8,32 @@
 $ch = curl_init();
 
 // set URL and other appropriate options
-curl_setopt($ch, CURLOPT_URL, "http://localhost:8000/emailffa/44/");
-
+curl_setopt($ch, CURLOPT_URL, "http://localhost:8000/emailffa/20/");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
 // grab URL and pass it to the browser
-curl_exec($ch);
+//curl_exec($ch);
+//curl_close($ch);
 
+
+$contents = curl_exec($ch);
+//$response_start = strPos($buffer, "<li>");
+//$response_end = strPos($buffer, "</li>", $response_start);
+//$temp_code = substr($buffer, $response_start + 4, ($response_end - $response_start - 4));
+
+//var_dump($temp_code);
+
+
+
+//while (strpos($contents, '<li>'))
+//{
+   // $start = strpos($contents, '<li>');
+    //$end = strpos($contents, '</li>', $start) + 7;
+    //$rstring = substr($contents, $start, $end - $start);
+    
+   // echo $rstring;
+//}
+    
 
 ?>
 
