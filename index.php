6da7be5a969812f00afeabe663c9348c5957bff4
minerva22 <minerva.moawad@gmail.com>
2017-06-29 09:42:03 +0300

    get the result from a specific URL without html code

--- index.php
+++ index.php
@@ -2,38 +2,51 @@
 
 //run python with shell exec()
 
-//$result= shell_exec("python -m http://localhost:8000/emailffa/36/");
-//var_dump($result); //output should be in here
+
+//http://php.net/manual/en/function.curl-exec.php
+
 // create a new cURL resource
 $ch = curl_init();
 
 // set URL and other appropriate options
-curl_setopt($ch, CURLOPT_URL, "http://localhost:8000/emailffa/20/");
+curl_setopt($ch, CURLOPT_URL, "http://localhost:8000/emailffa/36/");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
 // grab URL and pass it to the browser
 //curl_exec($ch);
 //curl_close($ch);
 
-
-$contents = curl_exec($ch);
-//$response_start = strPos($buffer, "<li>");
-//$response_end = strPos($buffer, "</li>", $response_start);
-//$temp_code = substr($buffer, $response_start + 4, ($response_end - $response_start - 4));
-
-//var_dump($temp_code);
+$buffer = curl_exec($ch);
 
 
 
-//while (strpos($contents, '<li>'))
-//{
-   // $start = strpos($contents, '<li>');
-    //$end = strpos($contents, '</li>', $start) + 7;
-    //$rstring = substr($contents, $start, $end - $start);
-    
-   // echo $rstring;
-//}
-    
+//http://php.net/manual/en/function.strip-tags.php
+
+
+$string  = strip_tags($buffer); 
+$string = str_replace("\r", '', $string);    // --- replace with empty space
+$string = str_replace("\n", '', $string);   // --- replace with space
+$string = str_replace("\t", '', $string);   // --- replace with space
+ $string = str_replace("&nbsp;", "\n", $string);  // --- replace &nbsp; with \n
+$string = trim(preg_replace('/ {2,}/', "\n", $string));  // ----- remove multiple spaces ----- 
+var_dump($string);
+
+
+//Result
+
+//minerva@minerva-pc:~/Desktop/programming/test/run_python_in_php$ php index.php 
+//string(235) "FFA Private Bank - Jobs EmailFFA Private Bank sal
+
+//FFA Private Bank Jobs EmailAudit Active ledger Cron: 20 15 1 * *
+//s.akiki@ffaprivatebank.com
+//m.moawad@ffaprivatebank.com
+//d.adada@ffaprivatebank.com 
+//Developed by: Minerva Moawadt - 2017"
+
+
+
+
+
 
 ?>
 
